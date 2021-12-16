4d159ff2e34b0078e792c5a9c2b59e3996a7f11f
Ed Pfromer <epfromer@gmail.com>
2021-12-16 11:23:52 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import bodyParser from 'body-parser'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import path from 'path'
 import generateUniqueId from './util'
 import serviceKeyCheck from './middleware'
@@ -12,65 +12,72 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
 // The status
-app.get('/ifttt/v1/status', serviceKeyCheck, (req, res) => {
+app.get('/ifttt/v1/status', serviceKeyCheck, (req: Request, res: Response) => {
   res.status(200).send()
 })
 
 // The test/setup endpoint
-app.post('/ifttt/v1/test/setup', serviceKeyCheck, (req, res) => {
-  res.status(200).send({
-    data: {
-      samples: {
-        actionRecordSkipping: {
-          create_new_thing: { invalid: 'true' },
+app.post(
+  '/ifttt/v1/test/setup',
+  serviceKeyCheck,
+  (req: Request, res: Response) => {
+    res.status(200).send({
+      data: {
+        samples: {
+          actionRecordSkipping: {
+            create_new_thing: { invalid: 'true' },
+          },
         },
       },
-    },
-  })
-})
-
-// Trigger endpoints
-app.post('/ifttt/v1/triggers/new_thing_created', (req, res) => {
-  const key = req.get('IFTTT-Service-Key')
-
-  if (key !== IFTTT_SERVICE_KEY) {
-    res.status(401).send({
-      errors: [
-        {
-          message: 'Channel/Service key is not correct',
-        },
-      ],
     })
   }
+)
 
-  const data = []
-  let numOfItems = req.body.limit
+// Trigger endpoints
+app.post(
+  '/ifttt/v1/triggers/new_thing_created',
+  (req: Request, res: Response) => {
+    const key = req.get('IFTTT-Service-Key')
 
-  if (typeof numOfItems === 'undefined') {
-    // Setting the default if limit doesn't exist.
-    numOfItems = 3
-  }
-
-  if (numOfItems >= 1) {
-    for (let i = 0; i < numOfItems; i += 1) {
-      data.push({
-        created_at: new Date().toISOString(), // Must be a valid ISOString
-        meta: {
-          id: generateUniqueId(),
-          timestamp: Math.floor(Date.now() / 1000), // This returns a unix timestamp in seconds.
-        },
+    if (key !== IFTTT_SERVICE_KEY) {
+      res.status(401).send({
+        errors: [
+          {
+            message: 'Channel/Service key is not correct',
+          },
+        ],
       })
     }
-  }
 
-  res.status(200).send({
-    data: data,
-  })
-})
+    const data = []
+    let numOfItems = req.body.limit
+
+    if (typeof numOfItems === 'undefined') {
+      // Setting the default if limit doesn't exist.
+      numOfItems = 3
+    }
+
+    if (numOfItems >= 1) {
+      for (let i = 0; i < numOfItems; i += 1) {
+        data.push({
+          created_at: new Date().toISOString(), // Must be a valid ISOString
+          meta: {
+            id: generateUniqueId(),
+            timestamp: Math.floor(Date.now() / 1000), // This returns a unix timestamp in seconds.
+          },
+        })
+      }
+    }
+
+    res.status(200).send({
+      data: data,
+    })
+  }
+)
 
 // Query endpoints
 
-app.post('/ifttt/v1/queries/list_all_things', (req, res) => {
+app.post('/ifttt/v1/queries/list_all_things', (req: Request, res: Response) => {
   const key = req.get('IFTTT-Service-Key')
 
   if (key !== IFTTT_SERVICE_KEY) {
@@ -116,33 +123,36 @@ app.post('/ifttt/v1/queries/list_all_things', (req, res) => {
 })
 
 // Action endpoints
-app.post('/ifttt/v1/actions/create_new_thing', (req, res) => {
-  console.log('create_new_thing')
+app.post(
+  '/ifttt/v1/actions/create_new_thing',
+  (req: Request, res: Response) => {
+    console.log('create_new_thing')
 
-  const key = req.get('IFTTT-Service-Key')
+    const key = req.get('IFTTT-Service-Key')
 
-  if (key !== IFTTT_SERVICE_KEY) {
-    res.status(401).send({
-      errors: [
+    if (key !== IFTTT_SERVICE_KEY) {
+      res.status(401).send({
+        errors: [
+          {
+            message: 'Channel/Service key is not correct',
+          },
+        ],
+      })
+    }
+
+    res.status(200).send({
+      data: [
         {
-          message: 'Channel/Service key is not correct',
+          id: generateUniqueId(),
         },
       ],
     })
   }
-
-  res.status(200).send({
-    data: [
-      {
-        id: generateUniqueId(),
-      },
-    ],
-  })
-})
+)
 
 // listen for requests :)
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.render('index.ejs')
 })
 
