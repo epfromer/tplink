bc0d5c4e18b6692dab54beddecfa1e42801996f6
Ed Pfromer <epfromer@gmail.com>
2021-12-17 08:06:48 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
 import path from 'path'
+import login from 'tplink-cloud-api'
 import serviceKeyCheck from './middleware'
 import generateUniqueId from './util'
 
@@ -70,47 +71,54 @@ app.post(
 )
 
 // query: list all things
-app.post('/ifttt/v1/queries/list_all_things', (req: Request, res: Response) => {
-  console.log('/ifttt/v1/queries/list_all_things')
-  if (req.get('IFTTT-Service-Key') !== IFTTT_SERVICE_KEY) {
-    console.log(req.get('IFTTT-Service-Key'), ' not valid')
-    res
-      .status(401)
-      .send({ errors: [{ message: 'Channel/Service key is not correct' }] })
-    return
-  }
-
-  const data = []
-  let numOfItems = req.body.limit
-
-  if (typeof numOfItems === 'undefined') {
-    // Setting the default if limit doesn't exist.
-    numOfItems = 3
-  }
-
-  if (numOfItems >= 1) {
-    for (let i = 0; i < numOfItems; i += 1) {
-      data.push({
-        created_at: new Date().toISOString(), // Must be a valid ISOString
-        meta: {
-          id: generateUniqueId(),
-          timestamp: Math.floor(Date.now() / 1000), // This returns a unix timestamp in seconds.
-        },
-      })
+app.post(
+  '/ifttt/v1/queries/list_all_things',
+  async (req: Request, res: Response) => {
+    console.log('/ifttt/v1/queries/list_all_things')
+    if (req.get('IFTTT-Service-Key') !== IFTTT_SERVICE_KEY) {
+      console.log(req.get('IFTTT-Service-Key'), ' not valid')
+      res
+        .status(401)
+        .send({ errors: [{ message: 'Channel/Service key is not correct' }] })
+      return
     }
+
+    const tplink = await login(process.env.TPLINK_USER, process.env.TPLINK_PWD)
+    const deviceList = await tplink.getDeviceList()
+    console.log(deviceList)
+
+    const data = []
+    let numOfItems = req.body.limit
+
+    if (typeof numOfItems === 'undefined') {
+      // Setting the default if limit doesn't exist.
+      numOfItems = 3
+    }
+
+    if (numOfItems >= 1) {
+      for (let i = 0; i < numOfItems; i += 1) {
+        data.push({
+          created_at: new Date().toISOString(), // Must be a valid ISOString
+          meta: {
+            id: generateUniqueId(),
+            timestamp: Math.floor(Date.now() / 1000), // timestamp in seconds
+          },
+        })
+      }
+    }
+
+    let cursor = null
+
+    if (req.body.limit == 1) {
+      cursor = generateUniqueId()
+    }
+
+    res.status(200).send({
+      data: data,
+      cursor: cursor,
+    })
   }
-
-  let cursor = null
-
-  if (req.body.limit == 1) {
-    cursor = generateUniqueId()
-  }
-
-  res.status(200).send({
-    data: data,
-    cursor: cursor,
-  })
-})
+)
 
 // action: create a new thing
 app.post(
