25ec78552660fab4e524dbc122918c91b77c4599
Ed Pfromer <epfromer@gmail.com>
2022-11-22 08:27:31 -0700

    u

--- src/index.ts
+++ src/index.ts
@@ -209,4 +209,4 @@ app.post('/ifttt/v1/actions/turn_device_off', (req: Request, res: Response) => {
 })
 
 const port = process.env.PORT || 80
-app.listen(port, () => console.log(`tplink running on PORT: ${port}`))
+app.listen(port, () => console.log(`tplink on port ${port}`))
