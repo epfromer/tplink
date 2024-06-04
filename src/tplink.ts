3ba25fd9e571b748e2df138ef47a536b7708e66c
Ed Pfromer <epfromer@gmail.com>
2024-06-04 15:31:33 -0600

    u

--- src/tplink.ts
+++ src/tplink.ts
@@ -1,8 +1,9 @@
 // import axios from 'axios'
 import * as dotenv from 'dotenv'
-import crypto from "node:crypto"
-import { Agent } from 'undici'
+import crypto from 'crypto';
+import https from 'https';
 import { v4 } from 'uuid'
+import axios from 'axios';
 
 dotenv.config()
 
@@ -11,76 +12,96 @@ const VERBOSE = process.env.VERBOSE === '1'
 
 let cachedDeviceList: Array<any> = []
 
-const connect = async () => {
-  const terminalUUID = v4()
-  let r
-  try {
-    r = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        method: 'login',
-        params: {
-          appType: 'Kasa_Android',
-          cloudUserName: process.env.TPLINK_USER,
-          cloudPassword: process.env.TPLINK_PWD,
-          terminalUUID,
-        },
-      }),
-      // @ts-ignore
-      dispatcher: new Agent({
-        connect: {
-          rejectUnauthorized: false,
-          secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT
-        }
-      })
-    })
-  } catch (error) {
-    console.error('error: connect fetch error', error)
-    return { terminalUUID: null, token: null }
-  }
-  const json: any = await r.json()
-  if (VERBOSE) console.log('success! connect json', json)
+/**
+ * Handle this problem with Node 18
+ * write EPROTO B8150000:error:0A000152:SSL routines:final_renegotiate:unsafe legacy renegotiation disabled
+ * see https://stackoverflow.com/questions/74324019/allow-legacy-renegotiation-for-nodejs/74600467#74600467
+ **/
+const allowLegacyRenegotiationforNodeJsOptions = {
+  httpsAgent: new https.Agent({
+    // for self signed you could also add
+    // rejectUnauthorized: false,
+    // allow legacy server
+    secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+  }),
+};
 
-  if (json && json.error_code && json.msg) {
-    console.error('error: connect error', json.error_code, json.msg)
-    return { terminalUUID: null, token: null }
-  }
-
-  const token =
-    json && json.result && json.result.token ? json.result.token : ''
-  return { terminalUUID, token }
-}
-
-// const connectAxios = async () => {
+// const connect = async () => {
 //   const terminalUUID = v4()
-//   let token = null
-
-//   await axios.post(url, {
-//     body: JSON.stringify({
-//       method: 'login',
-//       params: {
-//         appType: 'Kasa_Android',
-//         cloudUserName: process.env.TPLINK_USER,
-//         cloudPassword: process.env.TPLINK_PWD,
-//         terminalUUID,
+//   let r
+//   try {
+//     r = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
 //       },
-//     }),
-//   })
-//     .then(function (response) {
-//       console.log("success response = ", response);
-//       // token =
-//       //   response && response.result && response.result.token ? response.result.token : ''
+//       body: JSON.stringify({
+//         method: 'login',
+//         params: {
+//           appType: 'Kasa_Android',
+//           cloudUserName: process.env.TPLINK_USER,
+//           cloudPassword: process.env.TPLINK_PWD,
+//           terminalUUID,
+//         },
+//       }),
+//       // @ts-ignore
+//       dispatcher: new Agent({
+//         connect: {
+//           rejectUnauthorized: false,
+//           secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT
+//         }
+//       })
 //     })
-//     .catch(function (error) {
-//       console.log("error", error);
-//     });
+//   } catch (error) {
+//     console.error('error: connect fetch error', error)
+//     return { terminalUUID: null, token: null }
+//   }
+//   const json: any = await r.json()
+//   if (VERBOSE) console.log('success! connect json', json)
 
-//   console.log("terminalUUID, token", terminalUUID, token)
+//   if (json && json.error_code && json.msg) {
+//     console.error('error: connect error', json.error_code, json.msg)
+//     return { terminalUUID: null, token: null }
+//   }
+
+//   const token =
+//     json && json.result && json.result.token ? json.result.token : ''
 //   return { terminalUUID, token }
 // }
+
+const connectAxios = async () => {
+  const terminalUUID = v4()
+  let token = null
+
+  await axios({
+    ...allowLegacyRenegotiationforNodeJsOptions,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    data: {
+      method: 'login',
+      params: {
+        appType: 'Kasa_Android',
+        cloudUserName: process.env.TPLINK_USER,
+        cloudPassword: process.env.TPLINK_PWD,
+        terminalUUID,
+      },
+    },
+  })
+    .then(function (response) {
+      console.log("success response = ", response);
+      // token =
+      //   response && response.result && response.result.token ? response.result.token : ''
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
+
+  console.log("terminalUUID, token", terminalUUID, token)
+  return { terminalUUID, token }
+}
 
 export async function getDevices() {
   if (cachedDeviceList.length > 0) {
@@ -90,8 +111,8 @@ export async function getDevices() {
     return cachedDeviceList
   }
 
-  // const { terminalUUID, token } = await connectAxios()
-  const { terminalUUID, token } = await connect()
+  const { terminalUUID, token } = await connectAxios()
+  // const { terminalUUID, token } = await connect()
   if (!terminalUUID) {
     console.error('error: getDevices no tplink terminalUUID')
     return
